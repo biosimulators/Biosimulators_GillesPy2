d5c1fd0fc1c6f69b23285193fd5aa52f623d208f
Bilal Shaikh <bilalshaikh42@gmail.com>
2020-11-03 17:11:25 -0500

    add test omex files

--- tests/fixtures/makeOmex.sh
+++ tests/fixtures/makeOmex.sh
@@ -2,3 +2,7 @@
 cd BIOMD0000000028/
 zip -r test.omex ex1/ ex2/ manifest.xml
 mv test.omex  ../BIOMD0000000028.omex
+cd ..
+cd BIOMD0000000297/
+zip -r test.omex ex1/ ex2/ manifest.xml 
+mv test.omex  ../BIOMD0000000297.omex

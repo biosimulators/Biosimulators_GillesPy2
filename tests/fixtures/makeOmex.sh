9955387373670b93c6cc5eaa092c1502d214e162
Jonathan Karr <jonrkarr@gmail.com>
2020-11-06 01:47:07 -0500

    generalzing makeOmex script

--- tests/fixtures/makeOmex.sh
+++ tests/fixtures/makeOmex.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd BIOMD0000000028/
-zip -r test.omex ex1/ ex2/ manifest.xml
-mv test.omex  ../BIOMD0000000028.omex
-cd ..
-cd BIOMD0000000297/
-zip -r test.omex ex1/ ex2/ manifest.xml 
-mv test.omex  ../BIOMD0000000297.omex
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+examples=(BIOMD0000000028 BIOMD0000000297 BIOMD0000000297.edited)
+for example in "${examples[@]}"
+do
+    cd "${SCRIPT_DIR}/${example}/"
+    tempfile=tmp.omex
+    zip -r "${tempfile}" ex1/ ex2/ manifest.xml
+    mv "${tempfile}" "../${example}.omex"
+done

d132ffdf3e8c5270c15c25ed05e3974e07200e00
Jonathan Karr <jonrkarr@gmail.com>
2020-11-25 15:14:33 -0500

    updating

--- biosimulators_gillespy2/core.py
+++ biosimulators_gillespy2/core.py
@@ -7,7 +7,7 @@
 :License: MIT
 """
 
-from Biosimulations_utils.simulation.data_model import TimecourseSimulation
+from Biosimulations_utils.simulation.data_model import TimecourseSimulation, SimulationResultsFormat
 from Biosimulations_utils.simulator.utils import exec_simulations_in_archive
 import enum
 import gillespy2
@@ -234,7 +234,7 @@ def exec_simulation(model_filename, model_sed_urn, simulation, working_dir, out_
        simulation (:obj:`TimecourseSimulation`): simulation
        working_dir (:obj:`str`): directory of the SED-ML file
        out_filename (:obj:`str`): path to save the results of the simulation
-       out_format (:obj:`str`): format to save the results of the simulation (e.g., `csv`)
+       out_format (:obj:`SimulationResultsFormat`): format to save the results of the simulation (e.g., `HDF5`)
 
     Returns:
         :obj:`pandas.DataFrame`: predicted species counts/concentrations;
@@ -245,6 +245,12 @@ def exec_simulation(model_filename, model_sed_urn, simulation, working_dir, out_
             the simulation requires an unsupported algorithm or algorithm parameter, or the simulation
             start time is not zero
     '''
+    # check that model is encoded in SBML
+    if model_sed_urn != "urn:sedml:language:sbml":
+        format = model_sed_urn.split("language:")
+        raise InputError(
+            expression=format, message="Model language with URN '{}' is not supported".format(model_sed_urn))
+
     # check that simulation is a time course simulation
     if not isinstance(simulation, TimecourseSimulation):
         raise InputError(expression=simulation.__class__, message='{} is not supported'.format(simulation.__class__.__name__))
@@ -253,13 +259,12 @@ def exec_simulation(model_filename, model_sed_urn, simulation, working_dir, out_
     if simulation.model_parameter_changes:
         raise InputError(expression=simulation.model_parameter_changes, message='Model parameter changes are not supported')
 
-    # Read the model located at `model_filename` in the format
-    # with the SED URN `model_sed_urn`.
-    if model_sed_urn != "urn:sedml:language:sbml":
-        format = model_sed_urn.split("language:")
-        raise InputError(
-            expression=format, message="Model language with URN '{}' is not supported".format(model_sed_urn))
+    # check that the desired output format is supported
+    if out_format != SimulationResultsFormat.HDF5:
+        raise NotImplementedError("Simulation results format '{}' is not supported".format(out_format))
 
+    # Read the model located at `model_filename` in the format
+    # with the SED URN `model_sed_urn`.    
     # Convert SBML into a GillesPy2 model
     model, errors = gillespy2.import_SBML(model_filename, simulation.model.metadata.name)
     if model is None or errors:

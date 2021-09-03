2988f0d6814e46ce7e055e072f654d96cca0d920
Jonathan Karr <jonrkarr@gmail.com>
2021-09-02 23:02:14 -0400

    feat: updated to biosimulators-utils 0.1.116

--- biosimulators_gillespy2/core.py
+++ biosimulators_gillespy2/core.py
@@ -10,25 +10,25 @@
 from .data_model import KISAO_ALGORITHM_MAP
 from biosimulators_utils.combine.exec import exec_sedml_docs_in_archive
 from biosimulators_utils.config import get_config, Config  # noqa: F401
-from biosimulators_utils.log.data_model import CombineArchiveLog, TaskLog  # noqa: F401
+from biosimulators_utils.log.data_model import CombineArchiveLog, TaskLog, StandardOutputErrorCapturerLevel  # noqa: F401
 from biosimulators_utils.viz.data_model import VizFormat  # noqa: F401
 from biosimulators_utils.report.data_model import ReportFormat, VariableResults, SedDocumentResults  # noqa: F401
 from biosimulators_utils.sedml import validation
 from biosimulators_utils.sedml.data_model import (Task, ModelLanguage, UniformTimeCourseSimulation,  # noqa: F401
                                                   Variable, Symbol)
-from biosimulators_utils.sedml.exec import exec_sed_doc
+from biosimulators_utils.sedml.exec import exec_sed_doc as base_exec_sed_doc
 from biosimulators_utils.simulator.utils import get_algorithm_substitution_policy
 from biosimulators_utils.utils.core import raise_errors_warnings
 from biosimulators_utils.warnings import warn, BioSimulatorsWarning
 from kisao.data_model import AlgorithmSubstitutionPolicy, ALGORITHM_SUBSTITUTION_POLICY_LEVELS
 from kisao.utils import get_preferred_substitute_algorithm_by_ids
 import gillespy2
+import lxml
 import math
 import numpy
-import functools
 
 __all__ = [
-    'exec_sedml_docs_in_combine_archive', 'exec_sed_task',
+    'exec_sedml_docs_in_combine_archive', 'exec_sed_doc', 'exec_sed_task', 'preprocess_sed_task',
 ]
 
 
@@ -52,20 +52,65 @@ def exec_sedml_docs_in_combine_archive(archive_filename, out_dir, config=None):
             * :obj:`SedDocumentResults`: results
             * :obj:`CombineArchiveLog`: log
     """
-    sed_doc_executer = functools.partial(exec_sed_doc, exec_sed_task)
-    return exec_sedml_docs_in_archive(sed_doc_executer, archive_filename, out_dir,
+    return exec_sedml_docs_in_archive(exec_sed_doc, archive_filename, out_dir,
                                       apply_xml_model_changes=True,
                                       config=config)
 
 
-def exec_sed_task(task, variables, log=None, config=None):
+def exec_sed_doc(doc, working_dir, base_out_path, rel_out_path=None,
+                 apply_xml_model_changes=False,
+                 log=None, indent=0, pretty_print_modified_xml_models=False,
+                 log_level=StandardOutputErrorCapturerLevel.c, config=None):
+    """ Execute the tasks specified in a SED document and generate the specified outputs
+
+    Args:
+        doc (:obj:`SedDocument` or :obj:`str`): SED document or a path to SED-ML file which defines a SED document
+        working_dir (:obj:`str`): working directory of the SED document (path relative to which models are located)
+
+        base_out_path (:obj:`str`): path to store the outputs
+
+            * CSV: directory in which to save outputs to files
+              ``{base_out_path}/{rel_out_path}/{report.id}.csv``
+            * HDF5: directory in which to save a single HDF5 file (``{base_out_path}/reports.h5``),
+              with reports at keys ``{rel_out_path}/{report.id}`` within the HDF5 file
+
+        rel_out_path (:obj:`str`, optional): path relative to :obj:`base_out_path` to store the outputs
+        apply_xml_model_changes (:obj:`bool`, optional): if :obj:`True`, apply any model changes specified in the SED-ML file before
+            calling :obj:`task_executer`.
+        log (:obj:`SedDocumentLog`, optional): log of the document
+        indent (:obj:`int`, optional): degree to indent status messages
+        pretty_print_modified_xml_models (:obj:`bool`, optional): if :obj:`True`, pretty print modified XML models
+        log_level (:obj:`StandardOutputErrorCapturerLevel`, optional): level at which to log output
+        config (:obj:`Config`, optional): BioSimulators common configuration
+        simulator_config (:obj:`SimulatorConfig`, optional): tellurium configuration
+
+    Returns:
+        :obj:`tuple`:
+
+            * :obj:`ReportResults`: results of each report
+            * :obj:`SedDocumentLog`: log of the document
+    """
+    return base_exec_sed_doc(exec_sed_task, doc, working_dir, base_out_path,
+                             rel_out_path=rel_out_path,
+                             apply_xml_model_changes=apply_xml_model_changes,
+                             log=log,
+                             indent=indent,
+                             pretty_print_modified_xml_models=pretty_print_modified_xml_models,
+                             log_level=log_level,
+                             config=config)
+
+
+def exec_sed_task(task, variables, preprocessed_task=None, log=None, config=None):
     ''' Execute a task and save its results
 
     Args:
-       task (:obj:`Task`): task
-       variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
-       log (:obj:`TaskLog`, optional): log for the task
-       config (:obj:`Config`, optional): BioSimulators common configuration
+        task (:obj:`Task`): task
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        preprocessed_task (:obj:`object`, optional): preprocessed information about the task, including possible
+            model changes and variables. This can be used to avoid repeatedly executing the same initialization
+            for repeated calls to this method.
+        log (:obj:`TaskLog`, optional): log for the task
+        config (:obj:`Config`, optional): BioSimulators common configuration
 
     Returns:
         :obj:`tuple`:
@@ -83,6 +128,9 @@ def exec_sed_task(task, variables, log=None, config=None):
     if config.LOG and not log:
         log = TaskLog()
 
+    if preprocessed_task is None:
+        preprocessed_task = preprocess_sed_task(task, variables, config=config)
+
     model = task.model
     sim = task.simulation
 
@@ -95,14 +143,15 @@ def exec_sed_task(task, variables, log=None, config=None):
                               error_summary='Changes for model `{}` are not supported.'.format(model.id))
         raise_errors_warnings(*validation.validate_model_changes(task.model),
                               error_summary='Changes for model `{}` are invalid.'.format(model.id))
-        raise_errors_warnings(validation.validate_simulation_type(task.simulation, (UniformTimeCourseSimulation, )),
+        raise_errors_warnings(validation.validate_simulation_type(sim, (UniformTimeCourseSimulation, )),
                               error_summary='{} `{}` is not supported.'.format(sim.__class__.__name__, sim.id))
-        raise_errors_warnings(*validation.validate_simulation(task.simulation),
+        raise_errors_warnings(*validation.validate_simulation(sim),
                               error_summary='Simulation `{}` is invalid.'.format(sim.id))
         raise_errors_warnings(*validation.validate_data_generator_variables(variables),
                               error_summary='Data generator variables for task `{}` are invalid.'.format(task.id))
 
-    target_x_paths_ids = validation.validate_variable_xpaths(variables, task.model.source, attr='id')
+    model_etree = lxml.etree.parse(task.model.source)
+    target_x_paths_ids = validation.validate_target_xpaths(variables, model_etree, attr='id')
 
     # Read the SBML-encoded model located at `task.model.source`
     model, errors = gillespy2.import_SBML(task.model.source)
@@ -110,9 +159,8 @@ def exec_sed_task(task, variables, log=None, config=None):
         raise ValueError('Model at {} could not be imported:\n  - {}'.format(
             task.model.source, '\n  - '.join(message for message, code in errors)))
 
-    # Load the algorithm specified by `simulation.algorithm`
-    simulation = task.simulation
-    algorithm_kisao_id = simulation.algorithm.kisao_id
+    # Load the algorithm specified by `sim.algorithm`
+    algorithm_kisao_id = sim.algorithm.kisao_id
     algorithm_substitution_policy = get_algorithm_substitution_policy(config=config)
     exec_kisao_id = get_preferred_substitute_algorithm_by_ids(
         algorithm_kisao_id, KISAO_ALGORITHM_MAP.keys(),
@@ -123,10 +171,10 @@ def exec_sed_task(task, variables, log=None, config=None):
     if solver == gillespy2.SSACSolver and (model.get_all_events() or model.get_all_assignment_rules()):
         solver = gillespy2.NumPySSASolver
 
-    # Apply the algorithm parameter changes specified by `simulation.algorithm.parameter_changes`
+    # Apply the algorithm parameter changes specified by `sim.algorithm.parameter_changes`
     algorithm_params = {}
     if exec_kisao_id == algorithm_kisao_id:
-        for change in simulation.algorithm.changes:
+        for change in sim.algorithm.changes:
             parameter = algorithm.parameters.get(change.kisao_id, None)
             if parameter:
                 try:
@@ -161,17 +209,17 @@ def exec_sed_task(task, variables, log=None, config=None):
                     warn(msg, BioSimulatorsWarning)
 
     # Validate that start time is 0 because this is the only option that GillesPy2 supports
-    if simulation.initial_time < 0:
+    if sim.initial_time < 0:
         raise NotImplementedError(
-            'Negative initial simulation time {} is not supported. Initial time must be >= 0.'.format(simulation.initial_time))
+            'Negative initial simulation time {} is not supported. Initial time must be >= 0.'.format(sim.initial_time))
 
     # set the simulation time span
-    number_of_points = (simulation.output_end_time - simulation.initial_time) / \
-        (simulation.output_end_time - simulation.output_start_time) * simulation.number_of_points
+    number_of_points = (sim.output_end_time - sim.initial_time) / \
+        (sim.output_end_time - sim.output_start_time) * sim.number_of_points
     if number_of_points != math.floor(number_of_points):
         raise NotImplementedError('Time course must specify an integer number of time points')
     number_of_points = int(number_of_points)
-    timespan = numpy.linspace(simulation.initial_time, simulation.output_end_time, number_of_points + 1)
+    timespan = numpy.linspace(sim.initial_time, sim.output_end_time, number_of_points + 1)
     model.timespan(timespan)
 
     # determine allowed variable targets
@@ -205,8 +253,8 @@ def exec_sed_task(task, variables, log=None, config=None):
             ),
         ]))
 
-    # Simulate the model from ``simulation.start_time`` to ``simulation.output_end_time``
-    # and record ``simulation.number_of_points`` + 1 time points
+    # Simulate the model from ``sim.start_time`` to ``sim.output_end_time``
+    # and record ``sim.number_of_points`` + 1 time points
     results_dict = model.run(solver, **algorithm.solver_args, **algorithm_params)[0]
 
     # transform the results to an instance of :obj:`VariableResults`
@@ -214,15 +262,15 @@ def exec_sed_task(task, variables, log=None, config=None):
     parameters = model.get_all_parameters()
     for variable in variables:
         if variable.symbol:
-            variable_results[variable.id] = results_dict['time'][-(simulation.number_of_points + 1):]
+            variable_results[variable.id] = results_dict['time'][-(sim.number_of_points + 1):]
 
         elif variable.target:
             sbml_id = target_x_paths_ids[variable.target]
             dynamics = results_dict.get(sbml_id, None)
             if dynamics is None:
-                variable_results[variable.id] = numpy.full((simulation.number_of_points + 1,), parameters[sbml_id].value)
+                variable_results[variable.id] = numpy.full((sim.number_of_points + 1,), parameters[sbml_id].value)
             else:
-                variable_results[variable.id] = dynamics[-(simulation.number_of_points + 1):]
+                variable_results[variable.id] = dynamics[-(sim.number_of_points + 1):]
 
     # log action
     if config.LOG:
@@ -234,3 +282,21 @@ def exec_sed_task(task, variables, log=None, config=None):
 
     # return results and log
     return variable_results, log
+
+
+def preprocess_sed_task(task, variables, config=None):
+    """ Preprocess a SED task, including its possible model changes and variables. This is useful for avoiding
+    repeatedly initializing tasks on repeated calls of :obj:`exec_sed_task`.
+
+    Args:
+        task (:obj:`Task`): task
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        preprocessed_task (:obj:`PreprocessedTask`, optional): preprocessed information about the task, including possible
+            model changes and variables. This can be used to avoid repeatedly executing the same initialization for repeated
+            calls to this method.
+        config (:obj:`Config`, optional): BioSimulators common configuration
+
+    Returns:
+        :obj:`object`: preprocessed information about the task
+    """
+    pass
